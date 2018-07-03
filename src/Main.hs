04f742c07a96092f3fa64ce7dab3806812feeb9e
MM <mmayer@mayeranalytics.com>
2018-07-03 07:50:13 +0200

    fibo example

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,37 @@
 module Main where
 
+import           Data.Monoid (mempty, (<>))
 import           StreamTrans
+
+sumS :: Num a => Stream a -> a
+sumS = foldl'S (+) 0
+
+concatS :: Monoid m => Stream m -> m
+concatS = foldl'S (<>) mempty
+
+-- just something to play with
+fibo :: Int -> Stream Int
+fibo n = Stream step (1,1,n) where
+    step (_,_,0) = Done
+    step (a,b,i) = Yield a (b, a+b, i-1)
+
+-- | Take numeric difference of stream, drop first el.
+diff :: Num a => Stream a -> Stream a
+diff (Stream step s0) = Stream step' (s0, Nothing) where
+    step' (s, x) = case step s of
+        Done     -> Done
+        Skip s' -> Skip (s', x)
+        Yield a' s' -> case x of
+            Nothing -> Skip (s', Just a')
+            Just x  -> Yield (a'-x) (s', Just a')
+
+-- | a dumb showList
+showListS :: Show a => Stream a -> String
+showListS = show . toList
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  let fibStr = fibo 20
+  print $ lengthS fibStr
+  putStrLn $ "fibo=" <> showListS fibStr
+  putStrLn $ "diff=" <> showListS (diff fibStr)
